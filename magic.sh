5386391a37fe176196331967bb41e27f186f875f
raju <raju@kali.kali>
2022-10-07 18:53:43 +0530

    fixed removal of original file, and members.csv file

--- magic.sh
+++ magic.sh
@@ -7,13 +7,15 @@ while IFS=, read -r lastName firstName gender cid
 do
     echo "Making files for : $cid"
 
-    # for file in $files
-    # do
-    #     cp -v $file "${cid}_${file}"
-    # done
+    for file in $files
+    do
+        cp -v $file "${cid}_${file}"
+    done
 
 done < $csv
 
+
+# For the last unfetched entry
 lastEntry=`tail -1 $csv`
 colunmNum=0
 
@@ -21,11 +23,20 @@ for entry in ${lastEntry//,/ }
 do
     if [ $colunmNum -eq 3 ]; then
         echo "Making files for : $entry"
-        # for file in $files
-        # do
-        #     cp -v $file "${cid}_${file}"
-        # done
-        exit
+        for file in $files
+        do
+            cp -v $file "${cid}_${file}"
+        done
+        break
     fi
     ((colunmNum+=1))
+done
+
+
+# Removing Original files
+for file in $files
+do
+    echo "Removing original file : $file"
+    rm -v $file
+    rm -v $csv
 done
